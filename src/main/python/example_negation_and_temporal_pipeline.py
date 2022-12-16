5fa3a10844d2c33b1ab5799df9f94beed573e193
Johnsd11 <34665038+Johnsd11@users.noreply.github.com>
2022-12-16 15:44:37 -0500

    adding an additional pipeline that uses both negation and temporal

--- src/main/python/example_negation_and_temporal_pipeline.py
+++ src/main/python/example_negation_and_temporal_pipeline.py
@@ -3,6 +3,7 @@ from ctakes_pbj import pbj_receiver
 from ctakes_pbj.pbj_tools import pbj_pipeline
 from ctakes_pbj import pbj_sender
 from example_negation import ExampleNegation
+from example_temporal import ExampleTemporal
 from ctakes_pbj import arg_parser
 args = arg_parser.get_args()
 
@@ -13,6 +14,7 @@ def main():
 
     pipeline = pbj_pipeline.Pipeline()
     pipeline.add(ExampleNegation())
+    pipeline.add(ExampleTemporal())
     pipeline.add(pbj_sender.PBJSender(args.send_queue, args.host_name, args.port_name, args.password, args.username))
     pipeline.initialize()
     pbj_receiver.start_receiver(pipeline, args.receive_queue, args.host_name, args.port_name)
