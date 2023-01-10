9797af018580c348ab16417023723503c09cf923
Sean Finan <sean.finan@tch.harvard.edu>
2023-01-10 13:27:55 -0500

    Comments

--- src/main/python/example_negation_pipeline.py
+++ src/main/python/example_negation_pipeline.py
@@ -11,10 +11,13 @@ warnings.filterwarnings("ignore")
 
 def main():
 
+    # Create a new PBJ Pipeline, add a class that interacts with cNLPT to add Negation to Events. 
     pipeline = pbj_pipeline.Pipeline()
     pipeline.add(ExampleNegation())
+    # Add a PBJ Sender to the end of the pipeline to send the processed cas back to cTAKES and initialize the pipeline.
     pipeline.add(pbj_sender.PBJSender(args.send_queue, args.host_name, args.port_name, args.password, args.username))
     pipeline.initialize()
+    # Start a PBJ receiver to accept cas objects from Artemis and process them in the pipeline. 
     pbj_receiver.start_receiver(pipeline, args.receive_queue, args.host_name, args.port_name)
 
 
