8eefcbbe73c17d562ed240675850a0b8d3f35c77
Sean Finan <sean.finan@tch.harvard.edu>
2023-01-10 13:16:41 -0500

    Comments

--- src/main/python/example_negation_and_temporal_pipeline.py
+++ src/main/python/example_negation_and_temporal_pipeline.py
@@ -9,14 +9,17 @@ args = arg_parser.get_args()
 
 warnings.filterwarnings("ignore")
 
-
+# This shows the example_negation pipeline and the example_temporal pipeline functionality merged into a single pipeline.
 def main():
 
+    # Create a PBJ Pipeline, add both the ExampleNegation and ExampleTemporal processors.
     pipeline = pbj_pipeline.Pipeline()
     pipeline.add(ExampleNegation())
     pipeline.add(ExampleTemporal())
+    # Add a PBJ Sender to the end of the pipeline to send the cas back to cTAKES and initialize the pipeline.
     pipeline.add(pbj_sender.PBJSender(args.send_queue, args.host_name, args.port_name, args.password, args.username))
     pipeline.initialize()
+    # start the PBJ receiver to accept cas objects from Artemis and process them in the pipeline.
     pbj_receiver.start_receiver(pipeline, args.receive_queue, args.host_name, args.port_name)
 
 
