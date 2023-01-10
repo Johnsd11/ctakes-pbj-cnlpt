28a93dee5e248cae02ad86b61e3c6defc0e394d5
Sean Finan <sean.finan@tch.harvard.edu>
2023-01-10 13:49:38 -0500

    Comments

--- src/main/python/example_temporal_pipeline.py
+++ src/main/python/example_temporal_pipeline.py
@@ -11,10 +11,13 @@ warnings.filterwarnings("ignore")
 
 def main():
 
+    # Create a new PBJ Pipeline, add a class that interacts with cNLPT to add Temporal information.
     pipeline = pbj_pipeline.Pipeline()
     pipeline.add(ExampleTemporal())
+    # Add a PBJ Sender to the end of the pipeline to send the cas back to cTAKES and initialize the pipeline.
     pipeline.add(pbj_sender.PBJSender(args.send_queue, args.host_name, args.port_name, args.password, args.username))
     pipeline.initialize()
+    # Start the PBJ Receiver, listening to the Artemis broker and processing all received cas objects. 
     pbj_receiver.start_receiver(pipeline, args.receive_queue, args.host_name, args.port_name)
 
 
